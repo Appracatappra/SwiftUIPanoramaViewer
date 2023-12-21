60a99231e59cc8f091057322280b99756a191353
Kevin Mullins <kevin_mullins@appracatappra.com>
2023-12-21 14:41:56 -0600

    Initial Commit

---
+++ Tests/SwiftUIPanoramaViewerTests/SwiftUIPanoramaViewerTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SwiftUIPanoramaViewer
+
+final class SwiftUIPanoramaViewerTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
